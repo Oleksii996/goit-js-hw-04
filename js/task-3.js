95c98dab2ede007e5640b5de23de7c4c7ea314b0
oleksii ponomarenko <alexponomarenko996@gmail.com>
2025-11-03 00:14:23 +0100

    update code

--- js/task-3.js
+++ js/task-3.js
@@ -6,15 +6,23 @@
   const profile = {
     username: 'Jacob',
     playTime: 300,
+
+    getInfo() {
+      return `${profile.username} has ${this.playTime} active hours!`; //можна this, можна напряму
+    },
+
+    changeUsername(newName) {},
+
+    updatePlayTime(hours) {},
   };
 
   console.log(profile.getInfo()); // "Jacob has 300 active hours!"
 
   profile.changeUsername('Marco');
-  console.log(profile.getInfo()); // "Marco has 300 active hours!"
+  console.log(profile.getInfo()); // "Jacob has 300 active hours!"
 
   profile.updatePlayTime(20);
-  console.log(profile.getInfo()); // "Marco has 320 active hours!"
+  console.log(profile.getInfo()); // "Jacob has 300 active hours!"
 }
 
 //#endregion
